88ffc8d5a4f3c76ccaabb88930ee3cc6c3c28974
rohitisog <rohitisog@gmail.com>
2025-03-09 23:32:55 +0530

    add db

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import db from "./utils/db.js";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//import all routes
+import userRoutes from "./routes/user.routes.js"
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.BASE_URL,
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Cohort!");
+});
+
+app.get("/hitesh", (req, res) => {
+  res.send("Hitesh");
+});
+
+app.get("/piyush", (req, res) => {
+  res.send("Piyush!");
+});
+
+//connect to db
+db();
+
+//user routes
+app.use("/api/v1/users/", userRoutes)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
